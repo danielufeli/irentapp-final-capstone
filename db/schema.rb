3d0cffe25d4b8bdf920175645439f83d25ddda05
Daniel Ufeli <danielufeli@yahoo.com>
2022-06-25 16:50:40 +0100

    Add User Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_25_154503) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_25_154914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_25_154503) do
     t.date "startDate"
     t.date "endDate"
     t.integer "cost"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
