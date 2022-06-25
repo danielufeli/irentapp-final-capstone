c54263153fe2858252b1055b3b355057e4cf34d6
david-lafontant <lafontant.david@gmail.com>
2022-06-25 10:04:57 -0600

    Add foreign keys to tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_25_154914) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_25_160214) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_25_154914) do
     t.integer "capacity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_houses_on_user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
@@ -31,6 +33,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_25_154914) do
     t.integer "cost"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.index ["house_id"], name: "index_reservations_on_house_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_25_154914) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "houses", "users"
+  add_foreign_key "reservations", "houses"
+  add_foreign_key "reservations", "users"
 end
