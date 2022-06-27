fc768c75c36aa0253a9904c91e4268f87d0b4c29
Daniel Ufeli <danielufeli@yahoo.com>
2022-06-28 00:05:46 +0100

    Add Houses Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   # resources :reservations
-  resources :houses
+  # resources :houses
   namespace :api do
     namespace :v1 do
       resources :reservations
+      resources :houses
     end
   end
 end
